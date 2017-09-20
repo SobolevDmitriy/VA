3ea222417d3663361f6071eabcab735d8d43ce48
SobolevDmitriy <coboleff.dmitriy@yandex.ru>
2017-09-20 18:52:05 +0300

    Added the ability to change the parameters of the j-axis

--- main.py
+++ main.py
@@ -3,23 +3,32 @@ import math
 import numpy as np
 from PyQt5.QtWidgets import (QApplication, QWidget,
                              QTableWidgetItem, QMessageBox)
-from front import Ui_Form
+from PyQt5.QtGui import QIcon, QPixmap
+
+from front import Ui_widget
 
 
-class MainWindow(Ui_Form):
+class MainWindow(Ui_widget):
     '''
 
-    MainWidow class inherits appearance of the window from UI_Form
-    and realizes behavior all widgets, Heune's method
+    MainWidow class inherits appearance of the window from UI_Form,
+    and realizes behavior of all widgets.
+    Solve equation ******  phi'' + a*phi' + sin(phi) = j  *****
+    using the Heune's method, build volt-ampere characteristics of
+    of a Josephson junction.
     '''
 
     def __init__(self, form):
+        #initialization of the window shape
         self.setupUi(form)
+
         self.calc_btn.clicked.connect(self.calcslot)
 
     def heune(self, j):
-        self.phi = np.zeros(self.n, dtype='float64')
-        self.ksi = np.zeros(self.n, dtype='float64')
+        #memory allocation for the phi and ksi arrays
+        self.phi = np.zeros(self.n)
+        self.ksi = np.zeros(self.n)
+
         for i in range(self.n-1):
             phi_star = self.phi[i] + self.h*self.ksi[i]
             ksi_star = self.ksi[i] + self.h*self.f(self.phi[i], self.ksi[i], j)
@@ -32,20 +41,19 @@ class MainWindow(Ui_Form):
         return avarage_ksi
 
     def va(self):
-        self.START = 0
-        self.END = 3
-        self.STEP = 0.01
-        self.avarage_ksi = np.zeros(int((self.END-self.START)/self.STEP))
-        self.j = np.arange(self.START, self.END,self.STEP)
+        self.avarage_ksi = np.zeros(int((self.STOP-self.START)/self.STEP))
+        self.j = np.arange(self.START, self.STOP, self.STEP)
         for i, j in enumerate(self.j):
             self.avarage_ksi[i] = self.heune(j)
 
     def f(self, phi, ksi, j):
         return -self.a * ksi - math.sin(phi) + j
 
-    def drawplots(self):
+    def drawplot(self):
+        #calculating volt-ampere characteristics
         self.va()
 
+        #clearing plot and filling it with new data
         self.va_pw.getPlotItem().clear()
         self.va_pw.getPlotItem().setTitle("Volt-ampere characteristics graph")
         self.va_pw.getPlotItem().plot(self.j, self.avarage_ksi)
@@ -68,23 +76,30 @@ class MainWindow(Ui_Form):
                 self.a = float(self.a_le.text())
                 self.h = float(self.h_le.text())
                 self.n = int(self.n_le.text())
+                self.START = float(self.start_le.text())
+                self.STEP = float(self.step_le.text())
+                self.STOP = float(self.stop_le.text())
                 break
             except ValueError:
+                #if data had been entered incorrectly show error message
                 err_msg = QMessageBox()
                 err_msg.setWindowTitle('Error message')
                 err_msg.setText('Only numbers required')
                 err_msg.setStandardButtons(QMessageBox.Ok)
                 err_msg.exec_()
                 return
+
+        #setting time for Heune's method
         self.time = np.arange(0, self.n * self.h, self.h)
 
-        self.drawplots()
+        self.drawplot()
         self.drawtable()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = QWidget()
+    window.setWindowIcon(QIcon(QPixmap("LabIcon.ico")))
     ui = MainWindow(window)
     window.show()
     sys.exit(app.exec_())
